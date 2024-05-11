aeb847202705acd38c4d71ca055df09e7a0218c4
Slavikkq <yaroslavmanuljak@gmail.com>
2024-05-11 13:53:25 +0300

    typescript hw 01

---
+++ src/generics/4.ts
@@ -0,0 +1,16 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(
+  initialValues: User,
+  updateData: Partial<User>
+): User {
+  return {
+    ...initialValues,
+    ...updateData,
+  };
+}
